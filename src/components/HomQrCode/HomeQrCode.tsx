2e76ca04b3ac0495358e619d7fa9b9d184ff5ff8
prokittikun <pro.kittikun.2546@gmail.com>
2023-07-30 16:45:43 +0700

    Add: Show QRCode

--- src/components/HomQrCode/HomeQrCode.tsx
+++ src/components/HomQrCode/HomeQrCode.tsx
@@ -1,47 +1,35 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import generatePayload from "promptpay-qr";
-import QRCode from 'qrcode'
+import { useLocalStorage } from "usehooks-ts";
+import { QRCodeSVG } from "qrcode.react";
 
 function HomeQrCode() {
-  const payload = generatePayload("0828302070", {});
-  const options = { type: 'svg', color: { dark: '#003b6a', light: '#f7f8f7' } }
-  const qr = async () => {
-    await new Promise((resolve, reject) => {
-      QRCode.toString(payload, options, (err, svg) => {
-        if (err) return reject(err);
-        console.log(svg);
-        
-        resolve(svg);
-      });
-    });
-  };
+  const [promptPay, setPromptPay] = useLocalStorage<{
+    name: string;
+    phoneNumber: string;
+  }>("promptPay", { name: "", phoneNumber: "" });
 
-  useEffect(() => {
-    console.log(qr());
-    
-  }, [])
-  
-  
-  
-  
-  
   return (
     <>
       <div className="z-0 h-full w-full max-w-md bg-white p-1">
         <div className="h-full w-full max-w-md rounded-2xl bg-purple-950 p-3">
-          {/* <Image
-            src="https://static.vecteezy.com/system/resources/previews/002/557/391/original/qr-code-for-scanning-free-vector.jpg"
-            alt=""
-            className="ml-auto mr-auto mt-10 rounded-2xl"
-            width={150}
-            height={150}
-          /> */}
+          <QRCodeSVG
+            className="w-full"
+            height={200}
+            value={generatePayload(promptPay.phoneNumber, {
+              amount: 0,
+            })}
+          />
           <div className="mt-5 text-center">
             <input
               autoComplete="false"
               placeholder="หมายเลขพร้อมเพย์/เลขบัตรประชาชน"
               type="text"
+              value={promptPay.phoneNumber}
+              onChange={(e) => {
+                setPromptPay({"name": promptPay.name, "phoneNumber": e.target.value});
+              }}
               className="w-full rounded-lg border px-4 py-3 outline-none focus:ring-1"
             />
           </div>
